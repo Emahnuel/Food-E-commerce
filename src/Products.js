a50f28a36e7072fe66e72462b6a3915bcc6a5db0
Emahnuel <65620093+Emahnuel@users.noreply.github.com>
2024-02-22 21:13:48 +0100

    first commit

---
+++ src/Products.js
@@ -0,0 +1,146 @@
+import product1 from './assets/1.png';
+import product2 from './assets/2.png';
+import product3 from './assets/3.png';
+import product4 from './assets/4.png';
+import product5 from './assets/5.png';
+import product6 from './assets/6.png';
+import product7 from './assets/7.png';
+import product8 from './assets/8.png';
+import product9 from './assets/9.png';
+import product10 from './assets/10.png';
+import product11 from './assets/11.png';
+import product12 from './assets/12.png';
+import product13 from './assets/13.png';
+import product14 from './assets/14.png';
+import product15 from './assets/15.png';
+import product16 from './assets/16.png';
+import product17 from './assets/17.png';
+import product18 from './assets/18.png';
+import product19 from './assets/19.png';
+import product20 from './assets/20.png';
+
+export const Products = [
+    {
+        id: 1,
+        productName: "Pasta and Chicken",
+        price: 1500,
+        productImage: product1,
+    },
+    {
+        id: 2,
+        productName: "Eba, Soup and Chicken",
+        price: 2000,
+        productImage: product2,
+    },
+    {
+        id: 3,
+        productName: "Yam and Sauce ",
+        price: 1000,
+        productImage: product3,
+    },
+    {
+        id: 4,
+        productName: "Pack of Small Chops",
+        price: 1250,
+        productImage: product4,
+    },
+    {
+        id: 5,
+        productName: "Afang Soup and Garri",
+        price: 1500,
+        productImage: product5,
+    },
+    {
+        id: 6,
+        productName: "Amala ,gbegiri ,ewedu and some proteins",
+        price: 3000,
+        productImage: product6,
+    },
+    {
+        id: 7,
+        productName: "Coconut Fried Rice and Turkey",
+        price: 1500,
+        productImage: product7,
+    },
+    {
+        id: 8,
+        productName: "Ewa Agoyin and Bread" ,
+        price: 1000,
+        productImage: product8,
+    },
+    {
+        id: 9,
+        productName: "Meat Pie and Soft Drink" ,
+        price: 1000,
+        productImage: product9,
+    },
+    {
+        id: 10,
+        productName: "Plantain and Egg" ,
+        price: 2000,
+        productImage: product10,
+    },
+    {
+        id: 11,
+        productName: "Shawamar" ,
+        price: 2500,
+        productImage: product11,
+    },
+    {
+        id: 12,
+        productName: "Cat Fish Pepper Soup" ,
+        price: 5000,
+        productImage: product12,
+    },
+    {
+        id: 13,
+        productName: "Ofada Rice and Sauce" ,
+        price: 3000,
+        productImage: product13,
+    },
+    {
+        id: 14,
+        productName: "Pack of Puff Puff" ,
+        price: 1000,
+        productImage: product14,
+    },
+    {
+        id: 15,
+        productName: "Plate of Suya and Soft Drink" ,
+        price: 2500,
+        productImage: product15,
+    },
+    {
+        id: 16,
+        productName: "Moi Moi" ,
+        price: 1000,
+        productImage: product16,
+    },
+    {
+        id: 17,
+        productName: "Fufu and Egusi Soup" ,
+        price: 3000,
+        productImage: product17,
+    },
+    {
+        id: 18,
+        productName: "Ofada and Stew Sauce" ,
+        price: 2500,
+        productImage: product18,
+    },
+    {
+        id: 19,
+        productName: "Jollof Rice and Chicken" ,
+        price: 2000,
+        productImage: product19,
+    },
+    {
+        id: 20,
+        productName: "Abacha and Grilled Fish" ,
+        price: 1500,
+        productImage: product20,
+    },
+];
+
+
+export default Products;
